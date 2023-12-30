dc5c263daf714409164c43a0a2df8fc461ada1ac
Moshiur Rahman Taz Mirage <moshiur.mirage@gmail.com>
2023-12-30 22:23:26 +0600

    completed dashboard layout design setup with grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { Box, CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "@/scenes/Navbar"
+
+import Dashboard from "@/scenes/dashboard"
+import Navbar from "./scenes/navbar";
+
+
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
@@ -12,9 +16,9 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-            <Navbar/>
+          <Navbar/>
             <Routes>
-              <Route path="/" element={<div>Dashboard Route</div>} />
+              <Route path="/" element={<Dashboard/>} />
               <Route
                 path="/predictions"
                 element={<div>Predictions Route</div>}
